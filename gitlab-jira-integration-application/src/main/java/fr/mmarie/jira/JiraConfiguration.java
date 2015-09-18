692806780076f13841849e5ecfd1f3e85bd3f13a
maximilien marie <maximilien.marie@isen-lille.fr>
2015-09-18 22:34:29 +0200

    fix: missing json annotation

--- gitlab-jira-integration-application/src/main/java/fr/mmarie/jira/JiraConfiguration.java
+++ gitlab-jira-integration-application/src/main/java/fr/mmarie/jira/JiraConfiguration.java
@@ -1,5 +1,6 @@
 package fr.mmarie.jira;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
@@ -7,13 +8,16 @@ import javax.validation.constraints.NotNull;
 public class JiraConfiguration {
     @NotEmpty
     @NotNull
+    @JsonProperty
     private String username;
 
     @NotEmpty
     @NotNull
+    @JsonProperty
     private String password;
 
     @NotEmpty
     @NotNull
+    @JsonProperty
     private String url;
 }
