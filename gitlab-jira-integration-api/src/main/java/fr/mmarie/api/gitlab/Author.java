e04eef4cfc2c78b6cb020e5a2ba3885ca9d82710
maximilien marie <maximilien.marie@isen-lille.fr>
2015-09-19 19:03:10 +0200

    test(gitlab): add test for author api

--- gitlab-jira-integration-api/src/main/java/fr/mmarie/api/gitlab/Author.java
+++ gitlab-jira-integration-api/src/main/java/fr/mmarie/api/gitlab/Author.java
@@ -1,6 +1,7 @@
 package fr.mmarie.api.gitlab;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Data
 @ToString(of = "name")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Author {
     @JsonProperty("name")
     private String name;
---
+++ gitlab-jira-integration-api/src/test/java/fr/mmarie/api/gitlab/AuthorTest.java
@@ -0,0 +1,42 @@
+package fr.mmarie.api.gitlab;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static fr.mmarie.api.jira.Assertions.assertThat;
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AuthorTest {
+
+    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+    @Test
+    public void serializesToJSON() throws IOException {
+        final Author author = new Author("akraxx", "contact@mmarie.fr");
+
+        final String expected = MAPPER.writeValueAsString(
+                MAPPER.readValue(fixture("fixtures/gitlab/author.json"), Author.class));
+
+        assertThat(MAPPER.writeValueAsString(author)).isEqualTo(expected);
+    }
+
+    @Test
+    public void deserializesFromJSON() throws IOException {
+        final Author author = MAPPER.readValue(fixture("fixtures/gitlab/author.json"), Author.class);
+
+        assertThat(author)
+                .hasName("akraxx")
+                .hasEmail("contact@mmarie.fr");
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        final Author author = new Author("akraxx", "contact@mmarie.fr");
+
+        assertThat(author.toString()).isEqualTo("Author(name=akraxx)");
+    }
+}
