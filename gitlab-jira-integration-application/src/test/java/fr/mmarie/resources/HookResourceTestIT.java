124d3d18174ea32c3cd043fa1c60dba34e395674
Maximilien Marie (a567336) <maximilien.marie@worldline.com>
2015-10-01 10:52:25 +0200

    fix: tests  broken after adding auth

--- gitlab-jira-integration-application/src/test/java/fr/mmarie/resources/HookResourceTestIT.java
+++ gitlab-jira-integration-application/src/test/java/fr/mmarie/resources/HookResourceTestIT.java
@@ -2,12 +2,18 @@ package fr.mmarie.resources;
 
 import fr.mmarie.api.gitlab.Event;
 import fr.mmarie.core.IntegrationService;
+import fr.mmarie.core.auth.GitLabAuthFilter;
+import fr.mmarie.core.auth.GitLabAuthenticator;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
+import java.security.Principal;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -16,14 +22,23 @@ public class HookResourceTestIT {
 
     private static final IntegrationService INTEGRATION_SERVICE = mock(IntegrationService.class);
 
+    private static final String PASSWORD = "test-password";
+
     @ClassRule
     public static final ResourceTestRule resources = ResourceTestRule.builder()
             .addResource(new HookResource(INTEGRATION_SERVICE))
+            .addProvider(new AuthDynamicFeature(new GitLabAuthFilter.Builder()
+                    .setAuthenticator(new GitLabAuthenticator(PASSWORD))
+                    .setUnauthorizedHandler((s, s1) -> Response.status(Response.Status.UNAUTHORIZED).build())
+                    .setRealm("GitLab HOOK")
+                    .buildAuthFilter()))
+            .addProvider(new AuthValueFactoryProvider.Binder<>(Principal.class))
             .build();
 
     @Test
     public void callHookShouldReturnNothing() throws Exception {
         Response response = resources.client().target("/hook")
+                .queryParam("token", Base64.getEncoder().encodeToString(String.format("%s:%s", "test-svc", PASSWORD).getBytes()))
                 .request()
                 .post(Entity.json(new Event(Event.Type.PUSH)));
 
